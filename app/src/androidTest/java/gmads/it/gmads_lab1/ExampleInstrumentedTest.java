af434df82ccf0671f779847ccb07dd63b888cae8
Lorenzo Santolini <lorenzosantolini@mbp-di-lorenzo.home>
2018-03-23 18:08:42 +0100

    no message

--- app/src/androidTest/java/gmads/it/gmads_lab1/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/gmads/it/gmads_lab1/ExampleInstrumentedTest.java
@@ -20,6 +20,7 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
+        //ciao
 
         assertEquals("gmads.it.gmads_lab1", appContext.getPackageName());
     }
