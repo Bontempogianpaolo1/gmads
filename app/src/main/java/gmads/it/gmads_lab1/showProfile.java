4638e20c571b9b5bb6c1370aaf8a3b2772cdbbf4
mrfreak94 <bitrix9494@gmail.com>
2018-03-25 16:41:59 +0200

    2baa

--- app/src/main/java/gmads/it/gmads_lab1/showProfile.java
+++ app/src/main/java/gmads/it/gmads_lab1/showProfile.java
@@ -58,7 +58,6 @@ public class showProfile extends AppCompatActivity {
 
         modProfileButton.setOnClickListener(this::onModProfileClick);
 
-
     }
 
     private void onModProfileClick(View v) {
