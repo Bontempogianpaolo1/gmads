aa26633bd310091c044e103cc4a3880848ce6327
Lorenzo Santolini <santolini.lorenzo@gmail.com>
2018-04-20 16:12:12 +0200

    addbook improved

--- app/src/main/java/gmads/it/gmads_lab1/AddBook.java
+++ app/src/main/java/gmads/it/gmads_lab1/AddBook.java
@@ -1,6 +1,7 @@
 package gmads.it.gmads_lab1;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -12,6 +13,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.view.KeyEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,11 +24,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.animation.ScaleAnimation;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
+
+import static gmads.it.gmads_lab1.EditProfile.REQUEST_IMAGE_LIBRARY;
 
 public class AddBook extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -34,7 +42,9 @@ public class AddBook extends AppCompatActivity
     private static final int REQUEST_IMAGE_CAPTURE = 1888;
     private static final int ZBAR_CAMERA_PERMISSION = 1;
     private Bitmap barcodeBitmap;
-    private TextView isbnText;
+    private String ISBNcode;
+    private TextView textViewISBN;
+    private ImageView ISBNimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +71,29 @@ public class AddBook extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        EditText editText = (EditText) findViewById(R.id.textViewISBN);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    setISBNcode(editText.getText().toString());
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+        this.ISBNimage = findViewById(R.id.imageViewISBN);
+        ISBNimage.setOnClickListener(this::onClickImageView);
+        //findViewById(R.id.selectimage).setOnClickListener(this::onClickImage);
+
     }
+
+    public void setISBNcode(String isbn){
+        this.ISBNcode = isbn;
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -128,6 +160,53 @@ public class AddBook extends AppCompatActivity
         }
     }
 
+
+    private void onClickImageView(View v){
+        Tools t= new Tools();
+        //set popup
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            assert imm != null;
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+        v.getContext();
+        android.app.AlertDialog.Builder ad=t.showPopup(this,getString(R.string.takeImage),getString(R.string.selectGallery),getString(R.string.selectFromCamera));
+        ad.setPositiveButton("gallery",(vi,w)->{
+            Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            pickIntent.setType("image/*");
+            startActivityForResult(pickIntent, REQUEST_IMAGE_LIBRARY);
+        });
+        ad.setNegativeButton("photo",(vi,w)->{
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        });
+        ad.show();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+        //manage request image capture
+        if (requestCode == REQUEST_IMAGE_CAPTURE  && resultCode == RESULT_OK) {
+
+            Bundle imageUri = data.getExtras();
+            assert imageUri != null;
+
+            barcodeBitmap = (Bitmap) imageUri.get("data");
+            //manage request image from gallery
+        } else if ( requestCode==REQUEST_IMAGE_LIBRARY && resultCode == RESULT_OK) {
+            try{
+                final Uri imageUri = data.getData();
+                assert imageUri != null;
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                barcodeBitmap = BitmapFactory.decodeStream(imageStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /*
+
     @Override
     //-->function activated when a request is terminated
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -145,4 +224,5 @@ public class AddBook extends AppCompatActivity
             //manage request image from gallery
         }
     }
+    */
 }
