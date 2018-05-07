5d0307bceef577d7c6d85cce7881f837e6b5ea4f
Unknown <utente.1620@gmail.com>
2018-05-07 19:16:35 +0200

    spazio per l'hamburger e il tasto indietro

--- app/src/main/java/gmads/it/gmads_lab1/ProfileImageEffect.java
+++ app/src/main/java/gmads/it/gmads_lab1/ProfileImageEffect.java
@@ -67,7 +67,7 @@ public class ProfileImageEffect extends CoordinatorLayout.Behavior<CircleImageVi
             mStartXPosition = (int) (child.getX() + (child.getWidth() / 2));
 
         if (mFinalXPosition == 0)
-            mFinalXPosition = mContext.getResources().getDimensionPixelOffset(R.dimen.abc_action_bar_content_inset_material) + (finalHeight / 2);
+            mFinalXPosition = 4*mContext.getResources().getDimensionPixelOffset(R.dimen.abc_action_bar_content_inset_material) + (finalHeight / 2);
 
         if (mStartToolbarPosition == 0)
             mStartToolbarPosition = dependency.getY() + (dependency.getHeight()/2);
