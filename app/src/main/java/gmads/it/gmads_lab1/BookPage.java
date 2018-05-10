336ad2b04de0c0434000b382e7851e18d6e127cb
Gogo <crepaldigiorgio@hotmail.it>
2018-05-10 12:32:18 +0200

    Qualcosa c'è

--- app/src/main/java/gmads/it/gmads_lab1/BookPage.java
+++ app/src/main/java/gmads/it/gmads_lab1/BookPage.java
@@ -1,6 +1,7 @@
 package gmads.it.gmads_lab1;
 
 import android.content.ContextWrapper;
+import android.graphics.drawable.Drawable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewPager;
@@ -15,6 +16,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -25,13 +28,14 @@ public class BookPage extends AppCompatActivity {
     Toolbar toolbar;
     String bookId;
     Book book;
-    CollapsingToolbarLayout vTitle;
+    TextView vTitle;
     TextView vAuthor;
     TextView vRating;
     TextView vCategory;
     TextView distance;
-    ViewPagerAdapter bookImageAdapter;
-    ViewPager viewPager;
+    ImageView avatar;
+    //ViewPagerAdapter bookImageAdapter;
+    //ViewPager viewPager;
     List<String> images = new ArrayList<>();
 
     @Override
@@ -43,9 +47,9 @@ public class BookPage extends AppCompatActivity {
         findViews();
         getBookInfo();
 
-        viewPager= findViewById(R.id.book_image);
-        bookImageAdapter= new ViewPagerAdapter(BookPage.this,images);
-        viewPager.setAdapter(bookImageAdapter);
+        //viewPager= findViewById(R.id.book_image);
+        //bookImageAdapter= new ViewPagerAdapter(BookPage.this,images);
+        //viewPager.setAdapter(bookImageAdapter);
 
         cw = new ContextWrapper(getApplicationContext());
         //setSupportActionBar(toolbar);
@@ -55,10 +59,11 @@ public class BookPage extends AppCompatActivity {
 
     public void findViews(){
         vTitle = findViewById(R.id.book_title);
-        vAuthor = findViewById(R.id.book_author);
-        vRating = findViewById(R.id.book_rating);
-        vCategory = findViewById(R.id.book_category);
-        distance=findViewById(R.id.distance);
+        //vAuthor = findViewById(R.id.book_author);
+        //vRating = findViewById(R.id.book_rating);
+        //vCategory = findViewById(R.id.book_category);
+        //distance=findViewById(R.id.distance);
+        avatar = findViewById(R.id.avatar);
     }
 
     public void getBookInfo(){
@@ -69,14 +74,15 @@ public class BookPage extends AppCompatActivity {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         book = dataSnapshot.getValue(Book.class);
-                        vTitle.setTitle(book.getTitle());
-                        vAuthor.setText(book.getAuthor().get(0));
+                        vTitle.setText(book.getTitle());
+                        /*vAuthor.setText(book.getAuthor().get(0));
                         vRating.setText(Double.toString(book.getAvgRating()));
-                        vCategory.setText(book.getCategories().get(0));
+                        vCategory.setText(book.getCategories().get(0));*/
                        // distance.setText(String.valueOf(book.getDistance()/1000));
                        // distance.append(" Km");
-                        bookImageAdapter.clearUrl();
-                        bookImageAdapter.addUrl(book.getUrlimage());
+                        //bookImageAdapter.clearUrl();
+                        //bookImageAdapter.addUrl(book.getUrlimage());
+                        avatar.setImageDrawable(loadImageFromURL(book.getUrlimage(), "bookImage"));
 
                     }
 
@@ -85,5 +91,15 @@ public class BookPage extends AppCompatActivity {
 
                     }
                 });
+    }
+
+    public Drawable loadImageFromURL(String url, String name) {
+        try {
+            InputStream is = (InputStream) new URL(url).getContent();
+            Drawable d = Drawable.createFromStream(is, name);
+            return d;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
