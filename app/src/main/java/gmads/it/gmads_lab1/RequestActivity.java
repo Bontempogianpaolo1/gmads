17eb174148fed1bff9273405372dc25a76d33258
Unknown <bitrix9494@gmail.com>
2018-05-30 20:46:22 +0200

    fatta richieste

--- app/src/main/java/gmads/it/gmads_lab1/RequestActivity.java
+++ app/src/main/java/gmads/it/gmads_lab1/RequestActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,144 +17,79 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import android.app.Activity;
-import android.widget.ExpandableListView.OnChildClickListener;
-import android.widget.ExpandableListView.OnGroupClickListener;
-import android.widget.ExpandableListView.OnGroupCollapseListener;
-import android.widget.ExpandableListView.OnGroupExpandListener;
 
-public class RequestActivity extends Activity{
+
+import gmads.it.gmads_lab1.fragments.FragmentViewPagerAdapter;
+
+public class RequestActivity extends AppCompatActivity{
 
     ExpandableListAdapter listAdapter;
     ExpandableListView expListView;
     List<String> listDataHeader;
     HashMap<String, List<String>> listDataChild;
-
+    TabLayout tab;
+    ViewPager pager;
+    Request_1_othersReq r1=new Request_1_othersReq();
+    Request_2_myReq r2=new Request_2_myReq();
+    private void setViews() {
+        expListView = (ExpandableListView) findViewById(R.id.explv);
+        tab=findViewById(R.id.tabs);
+        pager= findViewById(R.id.viewPager);
+    }
     @Override
     protected void onCreate( @Nullable Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_home);
-        setViews();
-
+        // setContentView(R.layout.activity_home);
         setContentView(R.layout.activity_request);
-
+        setViews();
         // get the listview
-        expListView = (ExpandableListView) findViewById(R.id.explv);
 
-        // preparing list data
-        prepareListData();
+        FragmentViewPagerAdapter vadapter = new FragmentViewPagerAdapter(getSupportFragmentManager());
+        vadapter.addFragment(r1);
+        vadapter.addFragment(r2);
+        pager.setAdapter(vadapter);
+        tab.setupWithViewPager(pager);
+        tab.getTabAt(0).setText("my request");
+        tab.getTabAt(1).setText("other request");
 
-        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled( int position, float positionOffset, int positionOffsetPixels ) {
 
-        // setting list adapter
-        expListView.setAdapter(listAdapter);
-
-        // Listview Group click listener
-        expListView.setOnGroupClickListener(new OnGroupClickListener() {
+            }
 
             @Override
-            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-                //Toast.makeText(getApplicationContext(),"Group Clicked " + listDataHeader.get(groupPosition),Toast.LENGTH_SHORT).show();
-                //parent.expandGroup(groupPosition);
-                return false;
+            public void onPageSelected( int position ) {
+                switch (position) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged( int state ) {
+
             }
         });
-
-        // Listview Group expanded listener
-        expListView.setOnGroupExpandListener(new OnGroupExpandListener() {
-
-            @Override
-            public void onGroupExpand(int groupPosition) {
-                //Toast.makeText(getApplicationContext(), listDataHeader.get(groupPosition) + " Expanded", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // Listview Group collapsed listener
-        expListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
-
-            @Override
-            public void onGroupCollapse(int groupPosition) {
-                //Toast.makeText(getApplicationContext(),listDataHeader.get(groupPosition) + " Collapsed",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        // Listview on child click listener
-        /*expListView.setOnChildClickListener(new OnChildClickListener() {
-
-            @Override
-            public boolean onChildClick(ExpandableListView parent, View v,
-                                        int groupPosition, int childPosition, long id) {
-                // TODO Auto-generated method stub
-                Toast.makeText(
-                        getApplicationContext(),
-                        listDataHeader.get(groupPosition)
-                                + " : "
-                                + listDataChild.get(
-                                listDataHeader.get(groupPosition)).get(
-                                childPosition), Toast.LENGTH_SHORT)
-                        .show();
-                return false;
-            }
-        });*/
-    }
-
-        /*
-         * Preparing the list data
-         */
-        private void prepareListData() {
-            listDataHeader = new ArrayList<String>();
-            listDataChild = new HashMap<String, List<String>>();
-
-            // Adding child data
-            listDataHeader.add("Top 250");
-            listDataHeader.add("Now Showing");
-            listDataHeader.add("Coming Soon..");
-
-            // Adding child data
-            List<String> top250 = new ArrayList<String>();
-            top250.add("The Shawshank Redemption");
-            top250.add("The Godfather");
-            top250.add("The Godfather: Part II");
-            top250.add("Pulp Fiction");
-            top250.add("The Good, the Bad and the Ugly e bla bla bla bla ciao come va bla bla bla");
-            top250.add("The Dark Knight");
-            top250.add("12 Angry Men");
-
-            List<String> nowShowing = new ArrayList<String>();
-            nowShowing.add("The Conjuring");
-            nowShowing.add("Despicable Me 2");
-            nowShowing.add("Turbo");
-            nowShowing.add("Grown Ups 2");
-            nowShowing.add("Red 2");
-            nowShowing.add("The Wolverine");
-
-            List<String> comingSoon = new ArrayList<String>();
-            comingSoon.add("2 Guns");
-            comingSoon.add("The Smurfs 2");
-            comingSoon.add("The Spectacular Now");
-            comingSoon.add("The Canyons");
-            comingSoon.add("Europa Report");
-
-            listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-            listDataChild.put(listDataHeader.get(1), nowShowing);
-            listDataChild.put(listDataHeader.get(2), comingSoon);
-        }
-
-    private void setViews() {
-
     }
 
     @Override
-    protected void onStart() {
+    protected void onStart () {
         super.onStart();
     }
 
     @Override
-    protected void onStop() {
+    protected void onStop () {
         super.onStop();
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onDestroy () {
         super.onDestroy();
     }
 
@@ -170,5 +107,4 @@ public class RequestActivity extends Activity{
 
         Toast.makeText(getApplicationContext(), "Richiesta rifiutata",Toast.LENGTH_LONG).show();
     }
-
 }
